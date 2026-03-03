e7bb78fca82d8060cb7793775a7467875056d7e7
v_sshuairen <jimmyrss1102@gmail.com>
2026-03-03 10:06:07 +0800

    chore: 初始化 Vue 3 + TypeScript + Vite 项目

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const onMouseLeave = (e: MouseEvent) => {
+  console.log("onMouseLeave", e);
+};
+</script>
+
+<template>
+  <div class="card" @mouseleave="onMouseLeave">
+    <input type="text" />
+    输入框示例
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 20px;
+  background: #9f8a8a;
+}
+</style>
